88948ba0d0bc00cd1aae8d532db6d559c0c65dcf
Gabrielly <gabriellycs@al.insper.edu.br>
2023-12-06 09:35:06 -0300

    .

---
+++ js/perfil.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("proximoPasso").addEventListener("click", function(event) { //botao de proxima etapa
+
+
+        // infos perfil 1
+        var nome = document.getElementById("nome").value;
+        // Verificação de nome
+        if (nome === "") {
+            alert("Por favor, insira seu nome antes de prosseguir.");
+            event.preventDefault();
+            return;
+        }
+        var genero = document.getElementById("genero").value;
+        var dataNascimento = document.getElementById("dataNascimento").value;
+        //verificação de data
+        if (dataNascimento === "") {
+            alert("Por favor, insira sua data de nascimento antes de prosseguir.");
+            event.preventDefault();
+            return;
+        }
+        var localizacao = document.getElementById("localizacao").value;
+        //verificação de nascimento 
+        if (localizacao === "") {
+            alert("Por favor, insira sua localização antes de prosseguir.");
+            event.preventDefault();
+            return;
+        }
+
+
+        // Armazena infos 1 no localStorage
+        localStorage.setItem("nome", nome);
+        localStorage.setItem("genero", genero);
+        localStorage.setItem("dataNascimento", dataNascimento);
+        localStorage.setItem("localizacao", localizacao);
+
+
+        console.log("Valores armazenados no localStorage.");
+
+
+    });
+
+
+    //ESSA PORRA NAO TA FUNCIONANDO
+
+    // document.getElementById("proximoPasso2").addEventListener("click", function(event) { //botao de proxima etapa
+    //     event.preventDefault();
+
+    //     // infos perfil 2
+    //     var escolaridade = document.getElementById("escolaridade").value;
+    //     var ocupacao = document.getElementById("ocupacao").value;
+    //     var renda = document.getElementById("renda").value;
+
+    //     // Armazzena infos 2 no localStorage
+    //     localStorage.setItem("escolaridade", escolaridade);
+    //     localStorage.setItem("ocupacao", ocupacao);
+    //     localStorage.setItem("renda", renda);
+
+    //     console.log("Valores armazenados no localStorage.");
+
+    // });
+});
+
