d157a3d2e82f4c406b62550a922ddab66bb4b5bf
alis-on-araujo <alisonao@al.insper.edu.br>
2024-02-27 16:55:11 -0300

    javascript

--- js/perfil.js
+++ js/perfil.js
@@ -10,29 +10,29 @@ document.addEventListener("DOMContentLoaded", function() {
             event.preventDefault();
             return;
         }
-        var genero = document.getElementById("genero").value;
-        var dataNascimento = document.getElementById("dataNascimento").value;
-        //verificação de data
-        if (dataNascimento === "") {
-            alert("Por favor, insira sua data de nascimento antes de prosseguir.");
-            event.preventDefault();
-            return;
-        }
-        var localizacao = document.getElementById("localizacao").value;
-        //verificação de nascimento 
-        if (localizacao === "") {
-            alert("Por favor, insira sua localização antes de prosseguir.");
-            event.preventDefault();
-            return;
-        }
+        // var genero = document.getElementById("genero").value;
+        // var dataNascimento = document.getElementById("dataNascimento").value;
+        // //verificação de data
+        // if (dataNascimento === "") {
+        //     alert("Por favor, insira sua data de nascimento antes de prosseguir.");
+        //     event.preventDefault();
+        //     return;
+        // }
+        // var localizacao = document.getElementById("localizacao").value;
+        // //verificação de nascimento 
+        // if (localizacao === "") {
+        //     alert("Por favor, insira sua localização antes de prosseguir.");
+        //     event.preventDefault();
+        //     return;
+        // }
 
-        // Armazena infos 1 no localStorage
+        // // Armazena infos 1 no localStorage
         localStorage.setItem("nome", nome);
-        localStorage.setItem("genero", genero);
-        localStorage.setItem("dataNascimento", dataNascimento);
-        localStorage.setItem("localizacao", localizacao);
+        // localStorage.setItem("genero", genero);
+        // localStorage.setItem("dataNascimento", dataNascimento);
+        // localStorage.setItem("localizacao", localizacao);
 
-        console.log("Valores armazenados no localStorage.");
+        // console.log("Valores armazenados no localStorage.");
 
     });
 });
